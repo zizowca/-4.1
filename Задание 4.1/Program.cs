60c46b80a7432033c07e8a91c4dc67f1d440d40a
Zizowca <Zizowca@ZIZOWCA>
2021-11-24 22:31:43 +0300

    Добавьте файлы проекта.

---
+++ Задание 4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Введите число N больше 0");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int Sum = 0;
+            for (int n=1; n <= 2*N-1; n+=2)
+            {
+             Sum += n;
+            }
+            Console.WriteLine("{0}", Sum);
+            Console.ReadKey();
+        }
+    }
+}
